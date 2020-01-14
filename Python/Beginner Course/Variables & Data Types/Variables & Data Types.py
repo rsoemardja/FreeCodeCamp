1cff912d2aa7c0f36dc1716dc62509b79f27380d
rsoemardja <rsoemardja@gmail.com>
2020-01-14 14:54:59 +1300

    Python Beginner Day 1
    Hello World, Drawing a Shape And Variables & Data Types

---
+++ Python/Beginner Course/Variables & Data Types/Variables & Data Types.py
@@ -0,0 +1,31 @@
+# In Python you will dealing with a lot of DATA
+
+print("There once was a man named George, ")
+print("he was 70 years old. ")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
+
+# to change the name you have to go through the line
+print("He really liked the name John, ")
+print("but didn't like being 70.")
+
+# this is a variable for the character_name
+character_name = "John"
+# this is a variable for the character_age
+character_age = "35"
+
+print("There once was a man named " +character_name + ", ")
+print("he was " + character_age +"years old")
+print("He really liked the name"  +character_name + ", ")
+print("but didn't like being" + character_age +  ", ")
+character_name = "John"
+
+#There are 3 basic Data Types we can Store
+
+# String Values Stores Plain Text
+character_name = "Tom"
+#Number Values Stores ... Numbers
+character_age = 50
+# Boolean Value Stores True or False Values
+is_male = True
+is_Female = False
