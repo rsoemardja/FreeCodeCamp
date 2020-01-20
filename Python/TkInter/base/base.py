befefba842a1a6a2f73a5bd3a166f3b22f0f5a57
rsoemardja <rsoemardja@gmail.com>
2020-01-20 19:59:45 +1300

    FreeCodeCamp Tkinter
    Part 1 (Without Readme Notes)

---
+++ Python/TkInter/base/base.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+root = Tk()
+root.title('Learn To Code at Codemy.com')
+root.iconbitmap('c:/gui/codemy.ico')
+
+def open():
+    global my_img
+    top = TopLevel()
+    top.title('My Second Window')
+    top.iconbitmap('c:/gui/codemy.ico')
+    my_img = ImageTk.PhotoImage(Image.open("images/aspen.png"))
+    my_label = Label(top, image=my_img).pack()
+    btn2 = Button(top, text="close window", command=top.destroy).pack()
+
+btn = Button(root, text="Open Second Window", command=open).pack()
+
+mainloop()
