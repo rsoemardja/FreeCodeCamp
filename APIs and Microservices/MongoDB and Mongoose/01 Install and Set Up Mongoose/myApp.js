6fd0294895b4deec63c45b83d9ce19ae8ecbe49a
rsoemardja <rsoemardja@gmail.com>
2020-05-22 20:00:26 +1200

    Sort MongoDB and Mongoose

--- APIs and Microservices/MongoDB and Mongoose/01 Install and Set Up Mongoose/myApp.js
+++ APIs and Microservices/MongoDB and Mongoose/01 Install and Set Up Mongoose/myApp.js
@@ -14,7 +14,9 @@
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
-
+// My Code Here
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
