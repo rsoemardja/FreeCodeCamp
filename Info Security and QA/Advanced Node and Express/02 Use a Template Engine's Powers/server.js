764f9e2ce2717b6682e3d58ccdff804eec15283d
rsoemardja <rsoemardja@gmail.com>
2020-05-27 23:42:35 +1200

    FCC Node and Express
    Exercise 1-4 and prep files

--- Info Security and QA/Advanced Node and Express/02 Use a Template Engine's Powers/server.js
+++ Info Security and QA/Advanced Node and Express/02 Use a Template Engine's Powers/server.js
@@ -10,11 +10,13 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug')
+
 app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+    //Change the response to render the Pug template
+    res.render(process.cwd() + '/views/pug/index.pug', { title: 'Hello', message: 'Please login' });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+    console.log("Listening on port " + process.env.PORT);
 });
--- Info Security and QA/Advanced Node and Express/03 Set up Passport/server.js
+++ Info Security and QA/Advanced Node and Express/03 Set up Passport/server.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -10,11 +13,22 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.set('view engine', 'pug')
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+    //Change the response to render the Pug template
+    res.render(process.cwd() + '/views/pug/index.pug', { title: 'Hello', message: 'Please login' });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+    console.log("Listening on port " + process.env.PORT);
 });
