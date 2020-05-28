ac6ca2ce329993c31781ff177ef7c76d6f808a40
rsoemardja <rsoemardja@gmail.com>
2020-05-28 20:32:03 +1200

    fcc node

--- Info Security and QA/Advanced Node and Express/05 Implement the Serialization of a Passport User/server.js
+++ Info Security and QA/Advanced Node and Express/05 Implement the Serialization of a Passport User/server.js
@@ -2,7 +2,11 @@
 
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("passport");
+const db = require("mongodb");
+const ObjectID = require('mongodb').ObjectID;
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -10,11 +14,35 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "pug");
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+passport.deserializeUser((id, done) => {
+    db.collection("users").findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        done(null, null);
+    });
+});
+
 app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+    //Change the response to render the Pug template
+    res.render(process.cwd() + "/views/pug/index.pug", {
+        title: "Hello",
+        message: "Please login"
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+    console.log("Listening on port " + process.env.PORT);
 });
