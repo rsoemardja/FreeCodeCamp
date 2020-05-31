1919dac0e2f6cfb799dd03903cd9016fb9521d23
rsoemardja <rsoemardja@gmail.com>
2020-06-01 02:10:35 +1200

    7-11 FCC Node
    Completed but Won't Connect to the server

--- Info Security and QA/Advanced Node and Express/07 How to Use Passport Strategies/server.js
+++ Info Security and QA/Advanced Node and Express/07 How to Use Passport Strategies/server.js
@@ -1,20 +1,79 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const mongo = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
-});
+app.set('view engine', 'pug')
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if (err) {
+        console.log('Database error: ' + err);
+    } else {
+        console.log('Successful database connection');
+
+        passport.serializeUser((user, done) => {
+            done(null, user._id);
+        });
+
+        passport.deserializeUser((id, done) => {
+            db.collection('users').findOne(
+                { _id: new ObjectID(id) },
+                (err, doc) => {
+                    done(null, doc);
+                }
+            );
+        });
+
+        passport.use(new LocalStrategy(
+            function (username, password, done) {
+                db.collection('users').findOne({ username: username }, function (err, user) {
+                    console.log('User ' + username + ' attempted to log in.');
+                    if (err) { return done(err); }
+                    if (!user) { return done(null, false); }
+                    if (password !== user.password) { return done(null, false); }
+                    return done(null, user);
+                });
+            }
+        ));
+
+        app.route('/')
+            .get((req, res) => {
+                res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'login', showLogin: true });
+            });
+
+        app.route('/login')
+            .post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+                res.redirect('/profile');
+            });
+
+        app.route('/profile')
+            .get((req, res) => {
+                res.render(process.cwd() + '/views/pug/profile');
+            });
+
+        app.listen(process.env.PORT || 3000, () => {
+            console.log("Listening on port " + process.env.PORT);
+        });
+    }
 });
