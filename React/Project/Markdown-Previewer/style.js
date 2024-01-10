4ee01fa7ab2cf4ea325339ae56282ab1f9938b0c
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2024-01-10 19:47:13 +1100

    Markdown Previewer Finished

--- React/Project/Markdown-Previewer/style.js
+++ React/Project/Markdown-Previewer/style.js
@@ -1,40 +1,107 @@
-class Application extends React.Component {
+// eslint-disable-next-line no-unused-vars
+const projectName = 'markdown-previewer';
+
+// ALLOWS LINE BREAKS WITH RETURN BUTTON
+marked.setOptions({
+  breaks: true,
+  highlight: function (code) {
+    return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+  }
+});
+
+// INSERTS target="_blank" INTO HREF TAGS (required for Codepen links)
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+};
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdown: placeholder
+      markdown: placeholder,
+      editorMaximized: false,
+      previewMaximized: false
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
+    this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
   }
   handleChange(e) {
     this.setState({
       markdown: e.target.value
     });
   }
+  handleEditorMaximize() {
+    this.setState({
+      editorMaximized: !this.state.editorMaximized
+    });
+  }
+  handlePreviewMaximize() {
+    this.setState({
+      previewMaximized: !this.state.previewMaximized
+    });
+  }
   render() {
+    const classes = this.state.editorMaximized
+      ? ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress']
+      : this.state.previewMaximized
+      ? ['editorWrap hide', 'previewWrap maximized', 'fa fa-compress']
+      : ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
     return (
       <div>
-        <div className="row">
-          <div className="col-md-6">
-            <h1>Editor</h1>
-            <textarea
-              id="editor"
-              className="form-control"
-              value={this.state.markdown}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <h1>Preview</h1>
-            <div
-              id="preview"
-              dangerouslySetInnerHTML={{
-                __html: marked(this.state.markdown)
-              }}
-            />
-          </div>
+        <div className={classes[0]}>
+          <Toolbar
+            icon={classes[2]}
+            onClick={this.handleEditorMaximize}
+            text="Editor"
+          />
+          <Editor markdown={this.state.markdown} onChange={this.handleChange} />
+        </div>
+        <div className="converter" />
+        <div className={classes[1]}>
+          <Toolbar
+            icon={classes[2]}
+            onClick={this.handlePreviewMaximize}
+            text="Previewer"
+          />
+          <Preview markdown={this.state.markdown} />
         </div>
       </div>
     );
   }
 }
+
+const Toolbar = (props) => {
+  return (
+    <div className="toolbar">
+      <i className="fa fa-free-code-camp" title="no-stack-dub-sack" />
+      {props.text}
+      <i className={props.icon} onClick={props.onClick} />
+    </div>
+  );
+};
+
+const Editor = (props) => {
+  return (
+    <textarea
+      id="editor"
+      onChange={props.onChange}
+      type="text"
+      value={props.markdown}
+    />
+  );
+};
+
+const Preview = (props) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: marked(props.markdown, { renderer: renderer })
+      }}
+      id="preview"
+    />
+  );
+};
+
+const placeholder = `# Welcome to my React Markdown Previewer!`;
